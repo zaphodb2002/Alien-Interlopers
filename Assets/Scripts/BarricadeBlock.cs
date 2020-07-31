eb60cd708b239ab3ea0b12ce6f693ff667f966fd
Joshua Lindhardt <zaphodb2002@gmail.com>
2020-07-31 16:48:07 -0700

    New fonts, enemies moving as a single block, and barricades added.

---
+++ Assets/Scripts/BarricadeBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarricadeBlock : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Projectile" || collision.tag == "Enemy")
+        {
+            if (collision.tag == "Projectile")
+            {
+                Destroy(collision.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -10,10 +10,14 @@ public class EnemyManager : MonoBehaviour
     
     public float score = 0;
     [SerializeField] float baseSpeed = 1f;
-    
-
     [SerializeField] float step = 0.1f;
+    [SerializeField] int minimumBarricadeWidth = 16; //This includes 2 blocks on either side for padding
+    
     [SerializeField] Transform enemyPrefab = null;
+
+    [SerializeField] Transform barricadeBlockPrefab;
+
+
     [SerializeField] TextMeshProUGUI scoreUi;
     [SerializeField] TextMeshProUGUI waveUi;
 
@@ -36,6 +40,10 @@ public class EnemyManager : MonoBehaviour
         enemies = new List<Enemy>();
         speed = baseSpeed;
     }
+    private void Start()
+    {
+        CreateBarricades(3);
+    }
 
     private void Update()
     {
@@ -45,7 +53,7 @@ public class EnemyManager : MonoBehaviour
             {
                 speed = baseSpeed + (wavesActivated * step);
                 Player.instance.LevelUp();
-                CreateWave(1);
+                CreateWave(2);
                 wavesActivated++;
                 waveUi.text = wavesActivated.ToString();
             }
@@ -58,6 +66,7 @@ public class EnemyManager : MonoBehaviour
 
     }
 
+    Transform waveParent = null;
 
     private void CreateWave(int depth)
     {
@@ -71,25 +80,51 @@ public class EnemyManager : MonoBehaviour
             depth = Mathf.FloorToInt(GameManager.instance.ScreenHeightInUnits) - 3;
         }
 
-        int numOfEnemiesToSpawn = Mathf.FloorToInt(GameManager.instance.HalfScreenWidthInUnits);
-        Vector2 location = new Vector2(-Mathf.Floor(GameManager.instance.HalfScreenWidthInUnits), Mathf.Floor(GameManager.instance.ScreenHeightInUnits) - 2); //To Clear the score bar
-        for (int i = 0; i < numOfEnemiesToSpawn; i++)
+        int numOfEnemiesPerRow = Mathf.FloorToInt(GameManager.instance.HalfScreenWidthInUnits);
+        int initX = -Mathf.FloorToInt(GameManager.instance.HalfScreenWidthInUnits);
+        int initY = Mathf.FloorToInt(GameManager.instance.ScreenHeightInUnits) - depth - 1;  // extra 1 is to clear the UI stuff
+        Vector2 location = new Vector2(initX, initY);
+
+        waveParent = new GameObject("Wave Parent").transform;
+        waveParent.position = new Vector2((initX / 2f) - 0.5f, initY);
+        for (int j = 0; j < depth; j++)
         {
-            Transform newEnemy = Instantiate(enemyPrefab);
-            enemies.Add(newEnemy.GetComponent<Enemy>());
-            newEnemy.position = location;
-            location.x++;
+            for (int i = 0; i < numOfEnemiesPerRow; i++)
+            {
+                Transform newEnemy = Instantiate(enemyPrefab);
+                enemies.Add(newEnemy.GetComponent<Enemy>());
+                newEnemy.position = location;
+                location.x++;
+                newEnemy.parent = waveParent;
+            }
+            location.x = initX;
+            location.y++;
         }
+        
     }
 
+    bool isGoingRight = true;
     private void MoveEnemies(float speed)
     {
         if (enemies.Count > 0)
         {
-            foreach (var enemy in enemies)
+            Vector3 move = isGoingRight ? Vector3.right : Vector3.left;
+            
+            if (Mathf.Abs(waveParent.position.x) > (GameManager.instance.HalfScreenWidthInUnits / 2f) + 0.5f)
             {
-                enemy.DoMove(speed);
+                waveParent.position += Vector3.down;
+                waveParent.position += isGoingRight ? Vector3.left * 0.5f : Vector3.right * 0.5f;
+
+                isGoingRight = !isGoingRight;
             }
+            
+            waveParent.position += move * speed * Time.deltaTime;
+
+            if (waveParent.position.y <= 0)
+            {
+                DoPlayerDeath();
+            }
+
         }
     }
 
@@ -102,5 +137,33 @@ public class EnemyManager : MonoBehaviour
     public void RemoveEnemy(Enemy enemy)
     {
         enemies.Remove(enemy);
+    }
+
+    private void CreateBarricades(int thickness)
+    {
+        int screenWidthInBarricadeBlocks = Mathf.FloorToInt(GameManager.instance.ScreenWidthInUnits) * 4; // 4 because each barricade block is 0.25f;
+
+        int numberOfBarricades = screenWidthInBarricadeBlocks / minimumBarricadeWidth;
+
+        Vector3 location = new Vector3(-Mathf.FloorToInt(GameManager.instance.HalfScreenWidthInUnits), 1.25f);
+
+        for (int i = 0; i < numberOfBarricades; i++)
+        {
+            for (int block = 0; block < minimumBarricadeWidth; block++)
+            {
+                if (block > 1 && block < minimumBarricadeWidth - 2)
+                {
+                    for (int row = 0; row < thickness; row++)
+                    {
+                        var newBlock = Instantiate(barricadeBlockPrefab);
+                        newBlock.position = location;
+                        location.y += 0.25f;
+                    }
+                    location.y -= 0.25f * thickness;
+                }
+                
+                location.x += 0.25f;
+            }
+        }
     }
 }
